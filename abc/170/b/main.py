a38f47a499ea08f20d0a0cf6b9eda79b41d6ef80
Kaze Shindo <kaze.bknb@outlook.jp>
2020-11-24 23:52:59 +0900

    add 170-b

---
+++ abc/170/b/main.py
@@ -0,0 +1,18 @@
+def main():
+    X, Y = map(int, input().split())
+
+    # 1) X = turtle + crane
+    # 2) Y = turtle * 4 + crane * 2
+    # Y = X * 4 - crane * 2
+
+    crane  = (X*4 - Y) / 2
+    turtle = X - crane
+
+    if crane % 1 == 0 and turtle >= 0 and crane >= 0:
+        print('Yes')
+    else:
+        print('No')
+
+
+if __name__ == "__main__":
+    pass
